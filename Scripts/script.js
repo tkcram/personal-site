bc8454bb1164861a2be932f94e3d7e360e6b061d
Tk Cram <tkcram@tks-macbook.myfiosgateway.com>
2022-06-07 17:34:57 -0400

    Minor tweaks

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,23 +1,29 @@
 //Modal
 //Opening
 var modalWrapper = document.getElementById("modal-wrapper");
-var modalClose = document.getElementsByClassName("modal-content")
+var modalClose = document.getElementsByClassName("modal-content");
+var modalButton = document.getElementsByClassName("works-button-modal");
 
-var modalButton = document.getElementsByClassName("works-button-modal")
 for (var button of modalButton){
   const modalId = button.getAttribute('data-modal-id');
   button.addEventListener('click', () => {
     modalWrapper.style.display = "block";
-    var modalItem = document.getElementById(modalId)
+    var modalItem = document.getElementById(modalId);
     modalItem.style.display = "flex";
-  })
-}
+    window.location.hash = modalId;
+  });
+};
 
 document.addEventListener('DOMContentLoaded', function () {
-  var modalID = window.location.hash;
-  var modal = document.querySelector(modalID);
-  modalWrapper.style.display = "block";
-  modal.style.display = "flex";
+  var skipId = window.location.hash;
+  var skipType = skipId.split("-")[0];
+  var skipDiv = document.querySelector(skipId);
+  if(skipType == "#modal"){
+    modalWrapper.style.display = "block";
+    skipDiv.style.display = "flex";
+  } else if (skipType == "#section") {
+    skipDiv.scrollIntoView();
+  };
 }, false);
 
 //Closing
@@ -26,24 +32,27 @@ for (var button of closeButton){
   button.addEventListener('click', () => {
     modalWrapper.style.display = "none";
     for (var item of modalClose){
-      item.style.display = "none"
-    }
-  })
-}
+      item.style.display = "none";
+      window.location.hash = "";
+    };
+  });
+};
 window.onclick = function(event) {
   if (event.target == modalWrapper) {
     modalWrapper.style.display = "none";
     for (var item of modalClose){
-      item.style.display = "none"
-    }
-  }
-}
+      item.style.display = "none";
+      window.location.hash = "";
+    };
+  };
+};
 window.addEventListener('keydown', function (event) {
   if (event.key === 'Escape') {
-    modalWrapper.style.display = 'none'
+    modalWrapper.style.display = 'none';
     for (var item of modalClose){
-      item.style.display = "none"
-    }
-  }
-})
+      item.style.display = "none";
+      window.location.hash = "";
+    };
+  };
+});
 
